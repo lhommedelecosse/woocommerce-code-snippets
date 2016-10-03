291c05c1982cc1531649e56ab97a84aedc4bf4c7
Stuart Patterson <stuart@lhommedelecosse.com>
2016-10-03 10:14:13 +0200

    Update and rename woocommerce-display-12-products-per-page to woocommerce-display-12-products-per-page.php

--- woocommerce-display-12-products-per-page.php
+++ woocommerce-display-12-products-per-page.php
@@ -1,3 +1,5 @@
+<?php  // <~ do not copy the opening php tag
+
 // Display 12 products per page. Goes in functions.php
 add_filter( 'loop_shop_per_page', create_function( '$cols', 'return 12;' ), 20 );
 
