8ddf3f0d75235c6f15c01a0929c7d9d1772a2cdb
Stuart Patterson <stuart@lhommedelecosse.com>
2016-10-03 10:15:28 +0200

    Update and rename woocommerce-remove-products-count to woocommerce-remove-products-count.php

--- woocommerce-remove-products-count.php
+++ woocommerce-remove-products-count.php
@@ -1,3 +1,5 @@
+<?php  // <~ do not copy the opening php tag
+
 //* Removes products count after categories name 
 add_filter( 'woocommerce_subcategory_count_html', 'woo_remove_category_products_count' );
  
